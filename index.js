bec20abe7efa6ccdad5565fed009ea6f315783b4
Denis <dog123fri@mail.ru>
2022-05-06 06:31:31 +0300

    15 lessons

---
+++ index.js
@@ -0,0 +1,30 @@
+
+// let User = {
+    // firstName: "oleg",
+    // lastName: "lebovski",
+    // age: 25,
+    // group: "sleeps",
+    // speciality: "Datasience"
+// };
+
+// console.log (User);
+// 
+//  const result = confirm('Are you here?');
+//  console.log(result);
+// 
+//  const answer = prompt("Любишь радугу:?","yes")
+//  console.log(answer);
+
+//  let answers = [];
+// 
+//  answers[0] = prompt (" Как вас зовут?",'');
+//  answers[1] = prompt (" Сколько вам лет?",'');
+//  answers[2] = prompt ("Ваш пол ",'М/Ж');
+// 
+//  document.write(answers);
+//  console.log(typeof(answers));
+
+const user = 'Ivan';
+
+alert(`Привет, ${user}`);
+console.log ( "10" === 10);
